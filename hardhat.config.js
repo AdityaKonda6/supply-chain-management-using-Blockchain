a2f920db1b9d035f2a550f829d7558c4caf65bfd
Aditya Konda <adityakonda04@gmail.com>
2025-04-03 18:46:08 +0530

    modified:   hardhat.config.js 	modified:   package-lock.json 	modified:   package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-// You can use a .env file for better security
-// require("dotenv").config();
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    ganache: {
-      url: "http://127.0.0.1:7545", // Default Ganache URL (adjust if different)
-      // Instead of using a mnemonic, use a private key from your Ganache instance
-      accounts: [
-        "68752df3a213b9d4570e370801b8ccddbe2d5a6eb02c161e278e13570c73275c"
-      ]
-    },
-  },
-  paths: {
-    artifacts: "./artifacts",
-    sources: "./contracts",
-    cache: "./cache",
-    tests: "./test"
-  },
+    localhost: {
+      url: "http://127.0.0.1:7545",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
